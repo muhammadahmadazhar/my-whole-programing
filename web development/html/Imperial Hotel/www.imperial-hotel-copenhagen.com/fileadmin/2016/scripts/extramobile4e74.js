2b6b422525058c42362864b9e0bca0ba9a4cf61c
muhammadahmadazhar <muhammadahmadazhar16@gmail.com>
2019-11-07 21:38:05 +0500

    2 projects deleted

--- web development/html/Imperial Hotel/www.imperial-hotel-copenhagen.com/fileadmin/2016/scripts/extramobile4e74.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
- var windowwidth = window.screen.width < window.outerWidth ? window.screen.width : window.outerWidth;
- var mobilewidth = "1000";	
- if(windowwidth<mobilewidth){
-  $("#searchboxcon H1").clone().appendTo("#mobileh1"); 
-  $("#searchboxcon H1").remove();
-  }
-});
