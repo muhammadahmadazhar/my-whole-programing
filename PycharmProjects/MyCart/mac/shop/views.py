79bca364fd192cfda7908408de1741239bb4e63c
muhammadahmadazhar <muhammadahmadazhar16@gmail.com>
2019-12-09 03:54:23 +0500

    next task to create a cart

--- PycharmProjects/MyCart/mac/shop/views.py
+++ PycharmProjects/MyCart/mac/shop/views.py
@@ -4,10 +4,24 @@ from .models import product
 from math import ceil
 # Create your views here.
 def index(request):
-    products = product.objects.all()
-    n=len(products)
-    nSlides=n//4 + ceil((n/4)-(n//4))
-    params={'no of slides':nSlides,'range':range(1,nSlides),'product':products}
+    # products = product.objects.all()
+    # n=len(products)
+    # nSlides=n//4 + ceil((n/4)-(n//4))
+   # params={'no of slides':nSlides,'range':range(1,nSlides),'product':products}
+   #  allProds =[[products, range(1, nSlides), nSlides],
+   #             [products, range(1, nSlides), nSlides]]
+
+
+    allProds=[]
+    catProds = product.objects.values('category','id')
+    cats = {item['category'] for item in catProds}
+    for  cat in cats:
+        prod=product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1,nSlides), nSlides])
+
+    params = {'allProds': allProds}
     return render(request,'shop/index.html',params)
 
 def basic(request):
--- java programs/data structure java/recursion/1towerOfHanoi/src/towerofhanoi/TowerOfHanoi.java
+++ java programs/data structure java/recursion/1towerOfHanoi/src/towerofhanoi/TowerOfHanoi.java
@@ -1,12 +1,16 @@
 package towerofhanoi;
 class Toh{
+   static int c=0;
    public void tower(int n,char beg,char aux,char end){
+        c++;
          if(n==1){
-             System.out.println(beg+"->"+end);  
+             
+             System.out.println(n+":"+beg+"->"+end);  
          }
          else{//n>1
+             
              tower(n-1, beg, end, aux);
-             System.out.println(beg+"->"+end);
+             System.out.println(n+":"+beg+"->"+end);
              tower(n-1, aux, beg, end);
          }
     }
@@ -16,6 +20,7 @@ public class TowerOfHanoi {
     public static void main(String[] args) {
         Toh t=new Toh();
         t.tower(3,'A','B','C');
+        System.out.println("TOTAL MOVES="+Toh.c);
     }
     
 }
